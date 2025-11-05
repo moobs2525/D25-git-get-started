31fa89ddf4e10da129b3909e2bf8cb78dd106157
moobs2525 <micbon01@easv365.dk>
2025-11-05 13:19:58 +0100

    first change

---
+++ src/main/java/calculator.java
@@ -0,0 +1,12 @@
+public class calculator {
+    public static int sum ( int sumant1, int sumant2){
+        return sumant1 + sumant2;
+    }
+
+    public static int multiply(int factor1, int factor2){
+        return factor1 * factor2;
+    }
+    public static int devide(int num1, int num2){
+        return num1/num2;
+    }
+}
